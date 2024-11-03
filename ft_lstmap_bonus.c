121a011d721c00fb88039195f9bc43de9d279597
Zinebaiterrami1 <zineb.aiterrami1@gmail.com>
2024-11-03 21:36:35 +0100

    Bonus Part

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zait-err <zait-err@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 08:32:43 by zait-err          #+#    #+#             */
+/*   Updated: 2024/11/03 16:38:51 by zait-err         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*new_lst;
+	t_list	*tmp;
+
+	new_lst = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	tmp = lst;
+	while(tmp)
+	{
+		new = ft_lstnew(f(tmp->content));
+		if (!new)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new);
+		tmp = tmp->next;
+	}
+	return (new_lst);
+}
