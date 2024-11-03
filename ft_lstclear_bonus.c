ab9a725708ce55cab4b2c358b112ed9ca42d0203
Zinebaiterrami1 <zineb.aiterrami1@gmail.com>
2024-11-03 10:53:31 +0100

    Bonus Part

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zait-err <zait-err@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 08:27:42 by zait-err          #+#    #+#             */
+/*   Updated: 2024/10/30 08:28:00 by zait-err         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next_node;
+
+	if (!lst)
+		return ;
+	current = *lst;
+	while (current)
+	{
+		next_node = current->next;
+		ft_lstdelone(current, del);
+		current = next_node;
+	}
+	*lst = NULL;
+}
